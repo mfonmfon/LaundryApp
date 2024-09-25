767c073f571cf7051a35ef2cefa1d848f988796b
David-Bamgbade <davidbamgbade082@gmail.com>
2024-09-25 13:01:35 +0100

    created launderer login feature

--- src/main/java/com/semicolon/africa/laundryluxe/services/LaundererService.java
+++ src/main/java/com/semicolon/africa/laundryluxe/services/LaundererService.java
@@ -1,8 +1,11 @@
 package com.semicolon.africa.laundryluxe.services;
 
+import com.semicolon.africa.laundryluxe.dto.request.LoginLaundererRequest;
 import com.semicolon.africa.laundryluxe.dto.request.SignUpLaundererRequest;
+import com.semicolon.africa.laundryluxe.dto.response.LoginLaundererResponse;
 import com.semicolon.africa.laundryluxe.dto.response.SignUpCustomerResponse;
 
 public interface LaundererService {
     SignUpCustomerResponse signUp(SignUpLaundererRequest request);
+    LoginLaundererResponse loginLaunderer(LoginLaundererRequest request);
 }
